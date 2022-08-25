323578fc3e12225ea7398aae5802e156cb9f344f
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-08-26 00:04:19 +0530

    not done features

--- src/Pankaj_Components/AllMainDeatilsPage/DetailsPageModal.jsx
+++ src/Pankaj_Components/AllMainDeatilsPage/DetailsPageModal.jsx
@@ -1,15 +1,41 @@
-
 import React from 'react'
-import './HomePageDetails.css'
+import './HomePageDetails.css';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react'
 
 function DetailsPageModal() {
-  
-    return (
-      <>
-      
-      <div className="Details_Modal"> 
-      </div>
-      </>
-    )
-  }
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}  className="button">Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} className="modal_class" >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta aspernatur quia atque deserunt laudantium natus maiores totam quo, reiciendis exercitationem veritatis vel voluptas!
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 export default DetailsPageModal;
--- src/Pankaj_Components/AllMainDeatilsPage/DetailsRigthSide.jsx
+++ src/Pankaj_Components/AllMainDeatilsPage/DetailsRigthSide.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     FormControl,
     Input,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
     Button,
-    Box, 
-    useDisclosure
   } from '@chakra-ui/react';
 import './HomePageDetails.css'
 import DetailsPageModal from './DetailsPageModal';
 
 const DetailsRigthSide = () => {
-    // const navigate= useNavigate()
-    const handleModal = () => {
-        <DetailsPageModal />
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [cNumber, setCNumber] = useState('');
+    const [dDate, setDDate] = useState('');
+    const [destination, setDestination] = useState('');
+    const [dCity, setDCity] =useState('');
+    const [budget, setBudget] = useState('');
+
+    const [form,setForm] = useState([]);
+
+    const handleRequestCallback = (e) => {
+      e.preventDefault(e);
+      const payload = {
+        email,
+        name,
+        cNumber,
+        dDate,
+        destination,
+        dCity,
+        budget
+      }
+      setForm[payload];
+      // console.log(payload);
     }
+    console.log(form);
   return (
-    <div>
-      
+    <div> 
       <div className="left_siderbar_form">
       <h3>Want to book a similar trip?</h3>
-      <FormControl className="form">
-            <Input type="email" placeholder="Email" required  className="input_fild" />
-            <Input type="text" placeholder="Full Name" className="input_fild" required/>
-            <Input type="text" placeholder="Contact Number" className="input_fild" required/>
-            <Input placeholder="Departure Date" type="text" className="input_fild" required />
-            <Input type="text" placeholder="Departure City" className="input_fild" required/>
-            <Input type="text" placeholder="Destination" className="input_fild" required/>
-            <Input type="text" placeholder="Budget Per Person" className="input_fild" required/>
+      <FormControl className="form" >
+            <Input type="email" 
+            placeholder="Email"
+            required  
+            className="input_fild" 
+            onChange={(e) => setEmail(e.target.value)}/>
+
+            <Input type="text" placeholder="Full Name" className="input_fild" required
+             onChange={(e) => setName(e.target.value)}/>
+
+            <Input type="text" placeholder="Contact Number" className="input_fild" required 
+            onChange={(e) => setCNumber(e.target.value)}/>
+
+            <Input placeholder="Departure Date" type="text" className="input_fild" required
+            onChange={(e) => setDDate(e.target.value)} />
+
+            <Input type="text" placeholder="Departure City" className="input_fild" required
+            onChange={(e) => setDCity(e.target.value)}/>
+
+            <Input type="text" placeholder="Destination" className="input_fild" required 
+            onChange={(e) => setDestination(e.target.value)}/>
+
+            <Input type="text" placeholder="Budget Per Person" className="input_fild" required  
+            onChange={(e) => setBudget(e.target.value)}/>
 
             <div className="Fligth_book_radio">
                 <h4 style={{textAlign:'left'}}>Flights Booked?</h4>
@@ -48,8 +75,8 @@ const DetailsRigthSide = () => {
             </div>
 
             <div className="RequstCallbakc_button">
-                <Button className="RequstCallbakc_button" onClick={handleModal}>Request Callback</Button>
-                {/* <DetailsPageModal />           */}
+                <Button className="RequstCallbakc_button" onClick={handleRequestCallback}>Request Callback</Button>
+                <div><DetailsPageModal /></div>         
                 <p style={{fontSize:'10px'}}>I accept the Terms of Use and Privacy Policy of Tripoto.</p>
             </div>
       </FormControl>
